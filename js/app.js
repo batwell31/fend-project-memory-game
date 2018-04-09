247c56138b974987f776e11d20f3b6e9d3b7e04b
Brandon Lothaire Atwell <batwell31@yahoo.com>
2018-04-09 14:08:17 -0400

    Create variables to hold my querySelectors(to hold the value of an element)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 const cardList = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb", "fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
+const gameDeck = document.querySelector("#game-deck");
+const deck = document.querySelector("#deck");
+let movesElement = document.querySelector('#moves');
+const restart = document.querySelector('#restart');
+const timerElement = document.querySelector('#timer');
+const modal = document.getElementById('myModal');
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
